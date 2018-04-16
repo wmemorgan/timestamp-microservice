0eff4ed9fdda9fd87180d328126548fbb27f5755
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-16 19:48:30 +0800

    Add home page

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express'),
+      // url = require('url'),
       app = express(),
+      path = require('path'),
       port = process.env.PORT || 8080;
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+})
+
 app.listen(port, () => {
-  console.log("Server is live on port:", port);
+  console.log("Server is listening on port:", port);
+
 })
